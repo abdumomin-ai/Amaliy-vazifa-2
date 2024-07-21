468ba06b1e9dceff28c4a04cfd911b090b91c131
abdumomin-ai <155010267+abdumomin-ai@users.noreply.github.com>
2024-07-21 16:10:50 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        MyTime time = new MyTime(23, 59, 59);
+        MyTime time = new MyTime(00, 00, 00);
         System.out.println( time);
 
         time.nextSecond();
